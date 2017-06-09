761b57cd914e38d671c7b8365bc5fe1957aefb79
barbeu <yuriy.barbeu@gmail.com>
2017-06-09 11:33:55 +0300

    Added nfc read-write, main gridView.

--- app/src/main/java/com/example/tzadmin/nfc_reader_writer/MainActivity.java
+++ app/src/main/java/com/example/tzadmin/nfc_reader_writer/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.tzadmin.nfc_reader_writer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import com.example.tzadmin.nfc_reader_writer.Adapters.MainGridViewAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
+    String[] values = {
+            "Test1",
+    };
+    
+    int[] imageId = {
+            R.mipmap.ic_launcher,
+    };
+
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridView = (GridView) findViewById(R.id.gridView_main);
+
+        MainGridViewAdapter adapter = new MainGridViewAdapter(MainActivity.this, values, imageId);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view,
+                            int position, long id) {
+
+
     }
 }
