a5fc71b9b90a874d3686dc6605e801de96d92891
barbeu <yuriy.barbeu@gmail.com>
2017-06-22 16:40:34 +0300

    Add Shop activity.

---
+++ app/src/main/java/com/example/tzadmin/nfc_reader_writer/Adapters/ShopAdapter.java
@@ -0,0 +1,70 @@
+package com.example.tzadmin.nfc_reader_writer.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.tzadmin.nfc_reader_writer.Models.Shop;
+import com.example.tzadmin.nfc_reader_writer.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by forz on 22.06.17.
+ */
+
+public class ShopAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Shop> items;
+
+    public ShopAdapter(Context context, ArrayList<Shop> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View shop;
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            shop = inflater.inflate(R.layout.shop_item, null);
+
+            ImageView imageView = (ImageView)shop.findViewById(R.id.image_shop_item);
+            TextView name = (TextView) shop.findViewById(R.id.tv_shop_item_name);
+            TextView info = (TextView) shop.findViewById(R.id.tv_shop_item_info);
+            Button btn = (Button) shop.findViewById(R.id.btn_shop_item_add);
+
+            imageView.setImageResource(items.get(position).image);
+            name.setText(items.get(position).name);
+            info.setText(items.get(position).info);
+            //btn
+
+        } else {
+            shop = convertView;
+        }
+
+        return shop;
+    }
+}
---
+++ app/src/main/java/com/example/tzadmin/nfc_reader_writer/Models/Shop.java
@@ -0,0 +1,16 @@
+package com.example.tzadmin.nfc_reader_writer.Models;
+
+import android.media.Image;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by forz on 22.06.17.
+ */
+
+public class Shop {
+    public int image;
+    public String name;
+    public String info;
+    //public Button btn;
+}
