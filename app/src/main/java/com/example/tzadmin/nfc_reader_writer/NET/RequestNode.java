7420faf462aab488813ce1b23f0d160f62120a09
velor <v.khalilov@zonatelecom.ru>
2017-06-28 20:44:10 +0300

    add

--- app/src/main/java/com/example/tzadmin/nfc_reader_writer/NET/RequestNode.java
+++ app/src/main/java/com/example/tzadmin/nfc_reader_writer/NET/RequestNode.java
@@ -14,11 +14,13 @@ public class RequestNode {
     public String url;
     public RequestMethod method;
     public Map<String, String> params;
+    public Object backParam;
 
     void initIt() {
         url = "";
         method = RequestMethod.GET;
         params = new HashMap<>();
+        backParam = null;
     }
 
     public RequestNode(String url) {
@@ -28,6 +30,22 @@ public class RequestNode {
     public RequestNode(String url, RequestMethod method) {
         this.url = url;
         this.method = method;
+    }
+
+    public RequestNode(String url, RequestMethod method, Object backParam) {
+        this.url = url;
+        this.method = method;
+        this.backParam = backParam;
+    }
+
+    public RequestNode(String url, RequestMethod method, Object backParam, Pair<String, String>... params) {
+        this.url = url;
+        this.method = method;
+        this.backParam = backParam;
+
+        for (Pair<String, String> item : params) {
+            this.params.put(item.first, item.second);
+        }
     }
 
     public RequestNode(String url, RequestMethod method, Pair<String, String>... params) {
--- app/src/main/java/com/example/tzadmin/nfc_reader_writer/NET/Sync.java
+++ app/src/main/java/com/example/tzadmin/nfc_reader_writer/NET/Sync.java
@@ -1,5 +1,9 @@
 package com.example.tzadmin.nfc_reader_writer.NET;
 
+import com.example.tzadmin.nfc_reader_writer.Models.User;
+
+import java.util.Collection;
+
 /**
  * Created by velor on 6/27/17.
  *
@@ -31,11 +35,21 @@ package com.example.tzadmin.nfc_reader_writer.NET;
 
 public class Sync implements RequestDelegate {
 
-    private static final String ShopURL = "";
-    private static final String eventURL = "";
-    private static final String mordaURL = "";
-    private static final String groupURL = "";
-    private static final String routeURL = "";
+    private static final String shopURL = "http://194.67.194.82/sync/get-shop";
+    private static final String eventURL = "http://194.67.194.82/sync/get-events";
+    private static final String mordaURL = "http://194.67.194.82/sync/get-morda";
+    private static final String groupURL = "http://194.67.194.82/sync/get-group";
+    private static final String routeURL = "http://194.67.194.82/sync/get-route";
+    private static final String userURL = "http://194.67.194.82/sync/get-user";
+    private static final String moneyURL = "http://194.67.194.82/sync/get-money";
+    private static final String priorityURL = "http://194.67.194.82/sync/get-priority";
+    private static final String userMordaURL = "http://194.67.194.82/sync/get-user-morda";
+
+    private static final String setUserUrl = "http://194.67.194.82/sync/set-user";
+    private static final String setPriorityUrl = "http://194.67.194.82/sync/set-priority";
+    private static final String setMoneyUrl = "http://194.67.194.82/sync/set-money";
+    private static final String setUserMordaUrl = "http://194.67.194.82/sync/set-user-morda";
+
 
     /* стадия 1 -
         поиск и синхронизация таблиц пользователя
@@ -49,15 +63,20 @@ public class Sync implements RequestDelegate {
      */
     private int stage = 0;
 
-    public Sync () {
-        if (stage == 0) {
-            
-        }
-    }
-
-
-
-
+//    public Sync() {
+//        HttpRequester req = new HttpRequester();
+//
+//
+//        Collection<User> users = (Collection<User>) new User().selectAll();
+//
+//        RequestNode[] nodes = new RequestNode[users.size()];
+//
+//        int i = 0;
+//        for (User u : users) {
+//            //RequestNode node = new RequestNode(setUserUrl, RequestMethod.POST, u, u.getMap());
+//            i++;
+//        }
+//    }
 
     @Override
     public void RequestDone(String url, Object result) {
