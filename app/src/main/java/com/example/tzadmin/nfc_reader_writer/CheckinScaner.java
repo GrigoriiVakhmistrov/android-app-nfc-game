cec1fca3510eead866a6b419695cb1cbdff28d59
barbeu <yuriy.barbeu@gmail.com>
2017-06-28 09:21:36 +0300

    Call checkin menu.

--- app/src/main/java/com/example/tzadmin/nfc_reader_writer/CheckinScaner.java
+++ app/src/main/java/com/example/tzadmin/nfc_reader_writer/CheckinScaner.java
@@ -1,8 +1,11 @@
 package com.example.tzadmin.nfc_reader_writer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.tzadmin.nfc_reader_writer.Models.Activitys;
 
 public class CheckinScaner extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,7 +18,18 @@ public class CheckinScaner extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            //case R.id
+            case R.id.btn_chekin_spikers:
+                //startActivityForResult();
+                break;
+            case R.id.btn_chekin_activity:
+                startActivity(new Intent(this, Activitys.class));
+                break;
+            case R.id.btn_chekin_quest:
+                startActivity(new Intent(this, QuestActivity.class));
+                break;
+            case R.id.btn_chekin_route:
+                //startActivity(new Intent(this, .class));
+                break;
         }
     }
 }
