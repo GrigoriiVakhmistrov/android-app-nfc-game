60e3d15eeb40e1d6061133e2e7f537edf3bb6bc9
velor <v.khalilov@zonatelecom.ru>
2017-06-30 13:10:47 +0300

    Fix Spicker

--- app/src/main/java/com/example/tzadmin/nfc_reader_writer/SpickersActivity.java
+++ app/src/main/java/com/example/tzadmin/nfc_reader_writer/SpickersActivity.java
@@ -5,13 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.tzadmin.nfc_reader_writer.Messages.Message;
 import com.example.tzadmin.nfc_reader_writer.Models.Morda;
 import com.example.tzadmin.nfc_reader_writer.Models.User;
+import com.example.tzadmin.nfc_reader_writer.Models.UserMorda;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class SpickersActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,10 +23,22 @@ public class SpickersActivity extends AppCompatActivity implements View.OnClickL
     Morda spicker = null;
     boolean isSubscrube;
     String RfcId;
+
+    TextView text1;
+    TextView text2;
+    TextView text3;
+    TextView text4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spickers);
+
+        text1 = (TextView)findViewById(R.id.tv_checkin_spiker1);
+        text2 = (TextView)findViewById(R.id.tv_checkin_spiker2);
+        text3 = (TextView)findViewById(R.id.tv_checkin_spiker3);
+        text4 = (TextView)findViewById(R.id.tv_checkin_spiker4);
+
         findViewById(R.id.image_chekin_spicker1).setOnClickListener(this);
         findViewById(R.id.image_chekin_spicker2).setOnClickListener(this);
         findViewById(R.id.image_chekin_spicker3).setOnClickListener(this);
@@ -40,25 +55,33 @@ public class SpickersActivity extends AppCompatActivity implements View.OnClickL
             try {
                 if(spickers.get(0) == null)
                     findViewById(R.id.image_chekin_spicker1).setBackgroundResource(R.drawable.ic_spiker_not_found);
-                else
-                    Picasso.with(this).load(spickers.get(0).pic).into((ImageView)findViewById(R.id.image_chekin_spicker1));
+                else {
+                    Picasso.with(this).load(spickers.get(0).pic).placeholder(R.drawable.ic_spiker_not_found).into((ImageView) findViewById(R.id.image_chekin_spicker1));
+                    text1.setText(spickers.get(0).fio + "\n\n" + spickers.get(0).description);
+                }
 
                 if(spickers.get(1) == null)
                     findViewById(R.id.image_chekin_spicker2).setBackgroundResource(R.drawable.ic_spiker_not_found);
-                else
-                    Picasso.with(this).load(spickers.get(1).pic).into((ImageView)findViewById(R.id.image_chekin_spicker2));
+                else {
+                    Picasso.with(this).load(spickers.get(1).pic).placeholder(R.drawable.ic_spiker_not_found).into((ImageView) findViewById(R.id.image_chekin_spicker2));
+                    text2.setText(spickers.get(1).fio + "\n\n" + spickers.get(1).description);
+                }
 
 
                 if(spickers.get(2) == null)
                     findViewById(R.id.image_chekin_spicker3).setBackgroundResource(R.drawable.ic_spiker_not_found);
-                else
-                    Picasso.with(this).load(spickers.get(2).pic).into((ImageView)findViewById(R.id.image_chekin_spicker3));
+                else {
+                    Picasso.with(this).load(spickers.get(2).pic).placeholder(R.drawable.ic_spiker_not_found).into((ImageView) findViewById(R.id.image_chekin_spicker3));
+                    text3.setText(spickers.get(2).fio + "\n\n" + spickers.get(2).description);
+                }
 
 
                 if(spickers.get(3) == null)
                     findViewById(R.id.image_chekin_spicker4).setBackgroundResource(R.drawable.ic_spiker_not_found);
-                else
-                    Picasso.with(this).load(spickers.get(3).pic).into((ImageView)findViewById(R.id.image_chekin_spicker4));
+                else {
+                    Picasso.with(this).load(spickers.get(3).pic).placeholder(R.drawable.ic_spiker_not_found).into((ImageView) findViewById(R.id.image_chekin_spicker4));
+                    text4.setText(spickers.get(3).fio + "\n\n" + spickers.get(3).description);
+                }
             } catch (Exception ex) {
                 Toast.makeText(this,
                         "Ошибка URL картинки спикера, обратитесь к администратору",
@@ -102,8 +125,18 @@ public class SpickersActivity extends AppCompatActivity implements View.OnClickL
                     Toast.makeText(this,
                             Message.SUCCESSFULLY, Toast.LENGTH_SHORT).show();
                 } else {
-                    user.AddMoney(10, "Пользователь посетил семинар - " + spicker.fio);
-                    //todo
+                    Collection<Morda> subscribes = user.getSubscribed();
+
+                    for (Morda m : subscribes) {
+                        if (m.id.equals(spicker.id)) {
+                            user.AddMoney(10, "Пользователь посетил семинар - " + spicker.fio);
+                            Toast.makeText(this, "Пользователь успешно посетил семинар", Toast.LENGTH_LONG).show();
+                            finish();
+                            return;
+                        }
+                    }
+
+                    Toast.makeText(this, "Пользователь небыл подписан на семинар", Toast.LENGTH_LONG).show();
                     finish();
                 }
             } else {
